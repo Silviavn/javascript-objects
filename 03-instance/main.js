9c667615e4e84d38b649d22afccc0a2e2567151c
silvia <silviavergara.14sa@gmail.com>
2022-10-29 03:14:18 -0300

    pruebaf

--- 03-instance/main.js
+++ 03-instance/main.js
@@ -5,13 +5,14 @@ function Pokemon (name, height, hp, defense, attackpoints) {
     this.hp = hp,
     this.defense = defense,
     this.attackPoints = aP,
+    this.imgSrc = https : raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$%7Bthis.id%7D.png
     
     this.shhowStats = () => {
         return `Nombre : ${this.name}, Altura: ${this.height}`
     }
 }
-const pikachu = new Pokemon ("Pikachu", 4, 35, 40, 55)
-const charmander = new Pokemon ("Charmander", 6, 39, 52, 43)
+const pikachu = new Pokemon  (25, "Pikachu", 4, 35, 40, 55)
+const charmander = new Pokemon (4, "Charmander", 6, 39, 52, 43)
 
 pikachu.attack(charmander)
 charmander.attack(pikachu)
